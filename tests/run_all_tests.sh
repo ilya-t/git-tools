3dfeb5d2426262394d384b4f77740376ec1203ab
ilya-t <extraletters@gmail.com>
2021-08-31 23:10:16 +0300

    rull all tests at ci

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -2,15 +2,38 @@
 set -e
 
 mkdir -p ./reports/
+set +e
 cd ../builder
 ../toolsenv/bin/pytest --html=../tests/reports/builder_report.html --self-contained-html ./test_*.py
+BUILDER_RET_CODE=$?
 cd ../cleaner
 ../toolsenv/bin/pytest --html=../tests/reports/cleaner_report.html --self-contained-html ./test_*.py
+CLEANER_RET_CODE=$?
 cd ../switcher
 ../toolsenv/bin/pytest --html=../tests/reports/switcher_report.html --self-contained-html ./test_*.py
+SWITCHER_RET_CODE=$?
 cd ..
 
 echo "See test reports: "
 echo "  ./reports/builder_report.html"
 echo "  ./reports/cleaner_report.html"
 echo "  ./reports/switcher_report.html"
+
+RETCODE=0
+
+if [ "$BUILDER_RET_CODE" != "0" ]; then
+    echo "Builder tests failed"
+    RETCODE=1
+fi
+
+if [ "$CLEANER_RET_CODE" != "0" ]; then
+    echo "Cleaner tests failed"
+    RETCODE=1
+fi
+
+if [ "$SWITCHER_RET_CODE" != "0" ]; then
+    echo "Switcher tests failed"
+    RETCODE=1
+fi
+
+exit $RETCODE
