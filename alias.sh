42bc2d1410ccc20339d46ef56a3b9cc9690611a9
Ilya Tskhay <extraletters@gmail.com>
2021-09-10 13:33:12 +0300

    fix path to env's python binary

--- alias.sh
+++ alias.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 SCRIPT_DIR=$1
-PYTHON3=$SCRIPT_DIR/toolsenv/python3
+PYTHON3=$SCRIPT_DIR/toolsenv/bin/python3
 alias git-checkout="$PYTHON3 $SCRIPT_DIR/switcher/main.py "
 alias git-build="$PYTHON3 $SCRIPT_DIR/builder/workflow_updater.py "
 alias git-clean="$PYTHON3 $SCRIPT_DIR/cleaner/clean_merged.py"
